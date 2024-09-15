3afb6459bf9da269d41fc2ae0323b6008a14db90
isaackatana <mrisaackatana@gmail.com>
2024-09-15 10:41:05 +0300

    fullstack +

---
+++ front/src/main.jsx
@@ -0,0 +1,50 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import Artists from './pages/Artists.jsx'
+import Shop from './pages/Shop.jsx'
+import About from './pages/About.jsx'
+import NotFoundPage from './pages/NotFoundPage.jsx'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    
+    children: [
+      {
+        path: "/home",
+        element: <Home/>,
+      },
+      {
+        path: "/artists",
+        element: <Artists/>,
+      },
+      {
+        path: "/shop",
+        element: <Shop/>,
+      },
+      {
+        path: "/about",
+        element: <About/>,
+      },
+      {
+        path: "*",
+        element: <NotFoundPage/>,
+      },
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
