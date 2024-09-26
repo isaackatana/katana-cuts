fa61d0bd0def3f9edc461b275ddad7b9c3865aa7
isaackatana <mrisaackatana@gmail.com>
2024-09-26 10:40:57 +0300

    +++

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -6,9 +6,17 @@ function Home() {
     <div className="home">
     <iframe src="https://open.spotify.com/embed/playlist/2EIXrglBpVLUbXMylfnsGR?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     <div className="latest-videos">
+      <div className="container">
+      <div>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/YHESmYsFkrE?si=qcm2F5Io0HCUDXQc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+      <div>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/LxcmF_Kk0Ho?si=zrj-7KrPSRcVYsm_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/VyVVl_d2yZI?si=isaFRqOVs9blEZIN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+      <div>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/VyVVl_d2yZI?si=isaFRqOVs9blEZIN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+      </div>
       
     </div>
     </div>
